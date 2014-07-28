36c2155bad6adbb49f20e69139f8dddb2b12caa1
Jene Li <jene@aerospike.com>
2014-07-28 16:22:53 -0700

    clean is broken, examples added

--- src/python/expire_bin.py
+++ src/python/expire_bin.py
@@ -1,4 +1,5 @@
 import aerospike
+import time
 
 MODULE_NAME = "expire_bin"
 GET_OP = "get"
@@ -66,62 +67,113 @@ class ExpireBin:
 		except Exception as e:
 			raise
 
-		def puts(self, key, *binMaps):
-			"""Batch create or update expire bins for a given key. Use the dict
-				{'bin' : bin_name, 'val' : bin_value, 'bin_ttl' : ttl}
-			to store each put operation. Omit the bin_ttl to turn bin creation off.
+	def puts(self, key, *binMaps):
+		"""Batch create or update expire bins for a given key. Use the dict
+			{'bin' : bin_name, 'val' : bin_value, 'bin_ttl' : ttl}
+		to store each put operation. Omit the bin_ttl to turn bin creation off.
 
-			Args:
-				key -- tuple (namespace, set, record name)
-				*binMaps -- dict {'bin' : bin_name, 'val' : bin_val, 'bin_ttl' : ttl}
+		Args:
+			key -- tuple (namespace, set, record name)
+			*binMaps -- dict {'bin' : bin_name, 'val' : bin_val, 'bin_ttl' : ttl}
 
-			Returns:
-				int: 0 if all ops succeeded, 1 if error occurred
+		Returns:
+			int: 0 if all ops succeeded, 1 if error occurred
 
-			"""
-			try:
-				return self.client.apply(key, MODULE_NAME, BATCH_PUT_OP, list(binMaps), self.policy)
-			except Exception as e:
-				raise
+		"""
+		try:
+			return self.client.apply(key, MODULE_NAME, BATCH_PUT_OP, list(binMaps), self.policy)
+		except Exception as e:
+			raise
 
-		def touch(key, *mapBins):
-			"""Batch update the bin TTLs. Us this method to change or reset the bin TTL of
-			multiple bins in a record. Use a dict to store each touch operation.
+	def touch(self, key, *mapBins):
+		"""Batch update the bin TTLs. Us this method to change or reset the bin TTL of
+		multiple bins in a record. Use a dict to store each touch operation.
 
-			Args:
-				key -- tuple (namespace, set, record name)
-				*mapBins -- dict {'bin' : bin_name, 'bin_ttl' : ttl}
+		Args:
+			key -- tuple (namespace, set, record name)
+			*mapBins -- dict {'bin' : bin_name, 'bin_ttl' : ttl}
 
-			Returns:
-				int: 0 on success of ALL touch operations, 1 if a failure occurs
-			"""
-			try:
-				return self.client.apply(key, MODULE_NAME, TOUCH_OP, list(mapBins), self.policy)
-			except Exception as e:
-				raise
+		Returns:
+			int: 0 on success of ALL touch operations, 1 if a failure occurs
+		"""
+		try:
+			return self.client.apply(key, MODULE_NAME, TOUCH_OP, list(mapBins), self.policy)
+		except Exception as e:
+			raise
 
-		def clean(scan, *bins):
-			"""CLear out the expired bins on a scan of the database
+	def clean(self, scan, *bins):
+		"""Clear out the expired bins on a scan of the database
 
-			Args:
-				scan -- Scan object to run bin clean on
-				*bins -- bin names to clean out
-			"""
-			def clean_call((key, metadata, record)):
-				try:
-					self.client.apply(key, MODULE_NAME, CLEAN_OP, list(bins), self.policy)
-				except Exception as e:
-					raise
+		Args:
+			scan -- Scan object to run bin clean on
+			*bins -- bin names to clean out
+		"""
+		print "currently unimplemented"
+		##scan.select(bins)
+		##def clean_call(rec):
+		##	try:
+		##		self.client.apply(rec[0], MODULE_NAME, CLEAN_OP, list(bins), self.policy)
+		##	except Exception as e:
+		##		raise
 
-			scan.foreach(clean_call)
+		##scan.foreach(clean_call)
+
+	def ttl(self, key, bin):
+		"""Get the time bin will expire in seconds.
+
+		Args:
+			key -- tuple (namespace, set, record name)
+			bin -- bin name
+
+		Returns:
+			int: Time to expire in seconds
+		"""
+		try:
+			return self.client.apply(key, MODULE_NAME, TTL_OP, [bin], self.policy)
+		except Exception as e:
+			raise
+
 
 def main():
 	config = { 'hosts' : [ ('127.0.0.1', 3000) ]}
 	policy = { "timeout" : 2000 }
 	testClient = aerospike.client(config).connect()
 	eb = ExpireBin(testClient, policy)
-	key = ("test", "expireBin", "eb1")
+	key1 = ("test", "expireBin", "eb1")
+	key2 = ("test", "expireBin", "eb2")
+	key3 = ("test", "expireBin", "eb3")
 
+	print "Creating expire bins..."
+
+	print "TestBin 1: {0}".format("Inserted!" if eb.put(key1, "TestBin", "Hello World", -1, True) is 0 else "Insertion Failed")
+	print "TestBin 2: {0}".format("Inserted!" if eb.put(key2, "TestBin", "I don't expire", -1, True) is 0 else "Insertion Failed")
+	print "TestBin 3: {0}".format("Inserted!" if eb.put(key3, "TestBin", "I will expire soon", 5, True) is 0 else "Insertion Failed")
+
+	print "Getting expire bins..."
+
+	print "TestBin 1: {0}".format(eb.get(key1, "TestBin"))
+	print "TestBin 2: {0}".format(eb.get(key2, "TestBin"))
+	print "TestBin 3: {0}".format(eb.get(key3, "TestBin"))
+
+	print "Current time is: {0}\nGetting bin TTLs...".format(time.time())
+	print "TestBin 1 TTL: {0}", eb.ttl(key1, "TestBin")
+	print "TestBin 2 TTL: {0}", eb.ttl(key2, "TestBin")
+	print "TestBin 3 TTL: {0}", eb.ttl(key3, "TestBin")
+
+	print "Waiting for TestBin 3 to expire..."
+
+	time.sleep(10)
+
+	print "Getting expire bins again..."
+
+	print "TestBin 1: {0}".format(eb.get(key1, "TestBin"))
+	print "TestBin 2: {0}".format(eb.get(key2, "TestBin"))
+	print "TestBin 3: {0}".format(eb.get(key3, "TestBin"))
+
+	print "Cleaning bins..."
+
+	testScan = testClient.scan("test", "expireBin")
+	eb.clean(testScan, "TestBin")
 
 if __name__ == "__main__":
 	main()
