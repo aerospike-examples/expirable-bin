2db2ca4b763035d4bcb8f83b2be00ee1312eba96
Jene Li <jene@aerospike.com>
2014-08-14 15:08:24 -0700

    merged C client changes

--- src/c/expire_bin.c
+++ src/c/expire_bin.c
@@ -44,7 +44,12 @@
  * \return AEROSPIKE_OK if successful, otherwise an error.
  */
 as_status
-as_expbin_get(aerospike* as, as_error* err, const as_policy_apply* policy, const as_key* key, as_list* arglist, as_val** result)
+as_expbin_get(aerospike* as,
+		as_error* err,
+		const as_policy_apply* policy,
+		const as_key* key,
+		as_list* arglist,
+		as_val** result)
 {
 	as_status rc = aerospike_key_apply(as, err, policy, key, UDF_MODULE, "get", arglist, result);
 	if (rc != AEROSPIKE_OK) {
@@ -54,10 +59,10 @@ as_expbin_get(aerospike* as, as_error* err, const as_policy_apply* policy, const
 }
 
 /*
- * Create or update expire bins. If the create flag is set to true,
+ * Create or update expire bins. If bin_ttl is not NULL
  * 	all newly created bins will be expire bins otherwise, only normal bins will be created
  * 	and existing expire bins will be updated. Note: existing expire bins will not be converted
- * 	into normal bins if create flag is off.
+ * 	into normal bins if bin_ttl is NULL.
  *
  * \param as The aerospike instance to use for this operation.
  * \param err The as_error to be populated if an error occurs.
@@ -67,15 +72,21 @@ as_expbin_get(aerospike* as, as_error* err, const as_policy_apply* policy, const
  *   - bin name - char* with bin name
  *   - val - value to put inside bin
  *   - bin_ttl - expiration time in seconds or -1 for no expiration
- *   - create_flag: 0 to create expire bins, 1 to create normal bins
  * \param result - 0 if successfully written, 1 otherwise
  *
  * \return AEROSPIKE_OK if successful, otherwise an error.
  */
 as_status
-as_expbin_put(aerospike* as, as_error* err, const as_policy_apply* policy, const as_key* key, as_list* arglist, as_val** result)
+as_expbin_put(aerospike* as, as_error* err, const as_policy_apply* policy, const as_key* key, char* bin, as_val* val, int bin_ttl, as_val** result)
 {
-	as_status rc = aerospike_key_apply(as, err, policy, key, UDF_MODULE, "put", arglist, result);
+	as_arraylist arglist;
+	as_arraylist_inita(&arglist, 4);
+	as_arraylist_append_str(&arglist, bin);
+	as_arraylist_append(&arglist, val);
+	as_arraylist_append_int64(&arglist, bin_ttl);
+
+
+	as_status rc = aerospike_key_apply(as, err, policy, key, UDF_MODULE, "put", (as_list*)&arglist, result);
 	if (rc != AEROSPIKE_OK) {
 		LOG("as_expbin_put() returned %d - %s", err->code, err->message);
 	}
@@ -151,31 +162,35 @@ as_expbin_ttl(aerospike* as, as_error* err, const as_policy_apply* policy, const
 }
 
 /* 
- * Clear out the expired bins on a scan of the database
+ * Perform a background scan and remove all expired bins
  * 
  * \param as The aerospike instance to use for this operation.
- * \param ns The namespace to clean
- * \param set The set name to clean
- * \param arglist The list of bin names to clean.
+ * \param err The as_error to be populated if an error occurs
+ * \param policy The policy to use for this operation. If NULL, then the default policy will be used.
+ * \param scan as_scan to execute scan on
+ * \param binlist list of bins to clean
  *
- * \return AEROSPIKE_OK if successful, otherwise an error
+ * \return scan_id if successful, NULL if an error occurred.
  */
-as_status
-as_expbin_clean(aerospike* as, const char* ns, const char* set, as_list* arglist)
+uint64_t
+as_expbin_clean(aerospike* as, as_error* err, const as_policy_scan* policy, as_scan* scan, as_list* binlist)
 {
-	as_scan scan;
-	as_scan_init(&scan, ns, set);
-	as_scan_select_inita(&scan, as_list_size(arglist));
-	if (as_scan_apply_each(&scan, UDF_MODULE, "clean", arglist) != true) {
-		LOG("as_expbin_clean() failed");
+	uint64_t scan_id = NULL;
+	if (as_scan_apply_each(scan, UDF_MODULE, "clean", binlist) != true) {
+		LOG("UDF apply failed");
 	}
-	as_scan_destroy(&scan);
-	return AEROSPIKE_OK;
+	as_status rc = aerospike_scan_background(as, err, policy, scan, &scan_id);
+	if (rc != AEROSPIKE_OK) {
+		return NULL;
+	} else {
+		return scan_id;
+	}
 }
 
 //==========================================================
 // Expire Bin Example
 //  
+
 int
 main(int argc, char* argv[]) 
 {
@@ -197,29 +212,59 @@ main(int argc, char* argv[])
 	
 	as_key key1, key2, key3;
 	
-	printf("Creating expire bins...");
+	printf("Creating expire bins...\n");
 	
 	if (as_key_init_str(&key1, "test", "expireBin", "eb1") == NULL ||
-	    as_key_init_str(&key2, "test", "expireBin", "eb2") == NULL ||
+		as_key_init_str(&key2, "test", "expireBin", "eb2") == NULL ||
 		as_key_init_str(&key3, "test", "expireBin", "eb3") == NULL) {
 		printf("Keys were not initiated.\n");
 		exit(1);
 	}
-
-	as_arraylist arglist;
-	as_arraylist_inita(&arglist, 4);
-	as_arraylist_append_str(&arglist, "TestBin");
-	as_arraylist_append_str(&arglist, "Hello World!");
-	as_arraylist_append_int64(&arglist, -1);
-	as_arraylist_append_int64(&arglist, 0);
-
 	as_val* result = NULL;
+	as_string val;
+	as_string_init(&val, "Hello World.", false);
 
-	rc = as_expbin_put(&as, &err, NULL, &key1, (as_list*)&arglist, &result);
+	rc = as_expbin_put(&as, &err, NULL, &key1, "TestBin", (as_val*)&val, -1, &result);
+	if (rc != AEROSPIKE_OK) {
+		exit(1);
+	}
+
+	as_string_init(&val, "This is an expire bin.", false);
+
+	rc = as_expbin_put(&as, &err, NULL, &key2, "TestBin", (as_val*)&val, -1, &result);
+	if (rc != AEROSPIKE_OK) {
+		exit(1);
+	}
+
+	as_string_init(&val, "This bin will expire.", false);
+
+	rc = as_expbin_put(&as, &err, NULL, &key2, "TestBin", (as_val*)&val, 5, &result);
 	if (rc != AEROSPIKE_OK) {
 		exit(1);
 	}
 	
+	printf("Getting expire bins...\n");
+
+	as_arraylist arglist;
+	as_arraylist_inita(&arglist, 1);
+	as_arraylist_append_str(&arglist, "TestBin");
+
+	rc = as_expbin_get(&as, &err, NULL, &key1, (as_list*)&arglist, &result);
+
+	printf("TestBin 1: %s\n", as_string_tostring(as_string_fromval(result)));
+
+
+	rc = as_expbin_get(&as, &err, NULL, &key2, (as_list*)&arglist, &result);
+
+	printf("TestBin 2: %s\n", as_string_tostring(as_string_fromval(result)));
+
+
+	rc = as_expbin_get(&as, &err, NULL, &key3, (as_list*)&arglist, &result);
+
+	printf("TestBin 3: %s\n", as_string_tostring(as_string_fromval(result)));
+
+
+
 	aerospike_close(&as, &err);
 	aerospike_destroy(&as);
 
